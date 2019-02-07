f2e927ff74de8eae9e7cdbc181532230aa8a6e6a
Garrett Matthews <garrettematthews@gmail.com>
2019-02-07 11:16:34 -0700

    Updating test

--- Exams/Exam_1/Matthews_Skills_Test_1.R
+++ Exams/Exam_1/Matthews_Skills_Test_1.R
@@ -37,4 +37,16 @@ for(i in(levels(df$Year_Collected))){
 
 m = data.frame(Year = levels(df$Year_Collected), Ben.Mean = mb, Katy.Mean = mk )
 
-summary(m)
+print( m$Ben.Mean / m$Katy.Mean) -> dat
+df2 = data.frame(Year = levels(df$Year_Collected), Compare = dat)
+
+# To use the min function, I needed to convert the year column to a interger, however, this changed the
+# years values from being a year, to being 1-12.
+as.integer(df2$Year) -> df2$Year
+min(df2[df2$Compare, "Year"])
+# The min function told me what row had the smallest value of the compared means of Ben and Katy
+# I reset the data frame to redisplay the correct years, and then look at which year had the 
+# corresponding smallest value
+df2 = data.frame(Year = levels(df$Year_Collected), Compare = dat)
+df2[2,"Year"]
+
