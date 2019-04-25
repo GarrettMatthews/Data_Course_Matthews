e5dac1a79cb787a0044a20fc408ab6d61dae1173
GarrettMatthews <garrettemathews@gmail.com>
2019-04-25 11:20:15 -0600

    Updating files

--- Final_Project/Tree_Complete/phylo_test.R
+++ Final_Project/Tree_Complete/phylo_test.R
@@ -10,6 +10,10 @@ names = names[,1:4]
 labels= read.csv("./edge_label.csv", sep = " ")
 # Building the tree
 
+
+levels(names$Species)
+
+
 phang.align = as.phyDat(data, type = "DNA")
 
 dml = dist.ml(phang.align)
@@ -26,11 +30,8 @@ write.tree(fit$tree, "./Tree_Complete/tree.nwk")
 tree = read.tree("./Tree_Complete/tree.nwk")
 
 
-nme = as.character((rep(names$Genus,2)))
-nme = nme[1:460]
-
-clr = c("#ef2009","#085b03","#60453a","#7c7a79","#26ed23","#7c0947","#d88af2","#012602",
-        "#ccc42e","#0fc677","#0d0042","#b24f03","#05478e")
+clr = c("#ff6a00","#015600","#ff0000","#635c2b","#7a1c00","#7c0947","#a3007f","#ff4c7f",
+          "#0ab5b5","#08166d","#000000","#b24f03","#00a055")
 
 tr = ggtree(tree, layout = "circular", branch.length = 'none')
 tr = tr + geom_tiplab2(aes(color = labels$Genus), size = 4) + theme(legend.position = "right") + 
@@ -38,11 +39,10 @@ tr = tr + geom_tiplab2(aes(color = labels$Genus), size = 4) + theme(legend.posit
                                "Slime Mold", "Pine", "Gilled Mushrooms", "Oak",
                                "Yeast", "Tulip", "Moss") , values = clr)
 tr = tr + labs(title = expression(paste(italic("Quercus")) ~ "Species 18s Sequences Compared with other Eukaryotes"),
-          color = "Group") + theme(title = element_text(size = 20), legend.text = element_text(size = 15))
+          color = "Group") + theme(title = element_text(size = 20), legend.text = element_text(size = 15)) + guides(color = guide_legend(override.aes = list(size = 10)))
 
 tr
 ggsave("Tree_Complete/quercus_tree.pdf",tr, width = 50, height = 50, units = "cm", dpi = 1000, 
        limitsize = FALSE)                  
 
-factor(fit$tree$edge)
 
